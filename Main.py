ef4a5850a3c5dc0f6b611f541435ebbaf4405fe7
JulianWhite14 <juliandavidplazas28@gmail.com>
2026-04-05 14:46:00 -0500

    "Se implementó el registro del nombre y precio del producto"

--- Main.py
+++ Main.py
@@ -1,1 +1,5 @@
 print("Bienvenido a la Tienda Escolar")
+
+# Registrando la información del producto
+nombre_producto = input("Por favor, ingresa el nombre del producto: ")
+precio_producto = float(input("Por favor, ingresa el precio del producto: "))
