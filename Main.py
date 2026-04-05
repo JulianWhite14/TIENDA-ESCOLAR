aeb57768d2a520757481ef776f8d623461077e34
JulianWhite14 <juliandavidplazas28@gmail.com>
2026-04-05 14:51:55 -0500

    "Se añade un nueva variable para calcula el total de compra y se calcula el mismo"

--- Main.py
+++ Main.py
@@ -3,3 +3,9 @@ print("Bienvenido a la Tienda Escolar")
 # Registrando la información del producto
 nombre_producto = input("Por favor, ingresa el nombre del producto: ")
 precio_producto = float(input("Por favor, ingresa el precio del producto: "))
+
+# Nueva mejora: Pedir la cantidad
+cantidad_producto = int(input("¿Cuántas unidades deseas llevar?: "))
+
+# Calculando el total de la compra (La tarea de Carlos)
+total_compra = precio_producto * cantidad_producto
